06c57c54e21e93d4fc0680c8e7dfcfb6ded74bcf
Albert Lee <albertlee@Alberts-MacBook-Air.local>
2016-09-06 02:07:22 -0400

    changes

--- scripts/plotlyhtml.py
+++ scripts/plotlyhtml.py
@@ -1,5 +1,3 @@
-import clarity as cl  # I wrote this module for easier operations on data
-import clarity.resources as rs
 import csv,gc  # garbage memory collection :)
 
 import numpy as np
@@ -12,19 +10,24 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 ## Type in the path to your csv file here
-thedata = np.genfromtxt('../data/points/localeq.csv', delimiter=',', dtype='int', usecols = (0,1,2), names=['a','b','c'])
+thedata = np.genfromtxt('../csv/Fear199biglocaleq.brightest.csv', delimiter=',', dtype='int', usecols = (0,1,2), names=['a','b','c'])
 
 trace1 = go.Scatter3d(
     x = thedata['a'],
     y = thedata['b'],
     z = thedata['c'],
-    mode='markers',
-    marker=dict(
-        size=1.2,
-        color='purple',                # set color to an array/list of desired values
-        colorscale='Viridis',   # choose a colorscale
-        opacity=0.15
+    mode='lines',
+    line=dict(
+    width= 0,
+    colorscale= "Viridis",
+    opacity=0.1
     )
+    #marker=dict(
+     #   size=1.2,
+    #    color='purple',                # set color to an array/list of desired values
+    #    colorscale='Viridis',   # choose a colorscale
+    #    opacity=0.15
+    #)
 )
 
 data = [trace1]
@@ -39,4 +42,4 @@ layout = go.Layout(
     
 fig = go.Figure(data=data, layout=layout)
 print "localeq"
-plotly.offline.plot(fig, filename= "localeq")
+plotly.offline.plot(fig, filename= "linesopaque")
