94e358053b0ae3db0b66dd98a3f24a7c8707e10b
Albert Lee <lealbertle@gmail.com>
2016-09-12 03:00:45 -0400

    commit

---
+++ graphfiles/LukeGraphs/jsonconvert.py
@@ -0,0 +1,18 @@
+import json
+import networkx as nx
+G = nx.read_graphml('Fear199localeq.graphml')
+# add nodes, edges, etc to G ...
+
+def save(G, fname):
+    json.dump(dict(nodes=[[n, G.node[n]] for n in G.nodes()],
+                   edges=[[u, v, G.edge[u][v]] for u,v in G.edges()]),
+              open(fname, 'w'), indent=2)
+
+def load(fname):
+    G = nx.DiGraph()
+    d = json.load(open(fname))
+    G.add_nodes_from(d['nodes'])
+    G.add_edges_from(d['edges'])
+    return G
+
+save(G, 'Fear199.json')
