94d8e5441be2858b0ff4430732eb8f96cca41900
Jonathan Liu <jonathan.jy.liu@gmail.com>
2016-11-13 23:59:31 -0500

    debugging

--- django/seelviz/clarityviz/views.py
+++ django/seelviz/clarityviz/views.py
@@ -87,7 +87,8 @@ def token_compute(request):
         run_time = time.time() - start
         print('density_graph total time = %f' % run_time)
     
-    fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', 'output/' + token)
+    fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', token)
+    # fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', 'output/' + token)
     fzip_abs = os.path.abspath(fzip)
 
     print('just finished making zip')
