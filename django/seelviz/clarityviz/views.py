c8896dd760113390a55e1bacd3f13a9dd738c0b0
Jonathan Liu <jonathan.jy.liu@gmail.com>
2016-11-14 17:57:11 -0500

    bug fixes

--- django/seelviz/clarityviz/views.py
+++ django/seelviz/clarityviz/views.py
@@ -174,7 +174,7 @@ def token_compute(token, orientation, num_points=10000):
         run_time = time.time() - start
         print('density_graph total time = %f' % run_time)
     
-    fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', rootdir='output/' + token)
+    fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', root_dir='output/'+token)
     # fzip = shutil.make_archive('output/' + token + '/' + token, 'zip', 'output/' + token)
     fzip_abs = os.path.abspath(fzip)
 
