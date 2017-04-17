bbca8094be970868ca8e96de30f3545864dc4d43
lukezhu1 <lk.zhu12345@gmail.com>
2017-04-17 16:26:03 -0400

    updated tensor and tractography

--- algorithms/tractography.py
+++ algorithms/tractography.py
@@ -280,7 +280,9 @@ def generate_FSL_structure_tensor(img_data, filename, dogsigmaArr=[1], gausigmaA
 
             print "Saving a copy for this Gaussian sigma..."
             tensorfsl = np.concatenate((gprrrrgauss[..., np.newaxis], gprrccgauss[..., np.newaxis], gprrzzgauss[..., np.newaxis], gpccccgauss[..., np.newaxis], gpcczzgauss[..., np.newaxis], gpzzzzgauss[..., np.newaxis]), axis = 3);
-            
+            tmp = np.copy(tensorfsl[:,:,:,3])
+            tensorfsl[:,:,:,3] = tensorfsl[:,:,:,2]
+            tensorfsl[:,:,:,2] = tmp
              # Convert numpy ndarray object to Nifti data type
             tensor_fsl_data = nib.Nifti1Image(tensorfsl, affine=np.eye(4));
 
@@ -288,3 +290,100 @@ def generate_FSL_structure_tensor(img_data, filename, dogsigmaArr=[1], gausigmaA
 
     print 'Completed computing structure tensor on ' + str(filename) + '!'
     return tensorfsl
+
+
+def plot_rgb(im):
+    plt.rcParams.update({'axes.labelsize': 'x-large',
+                         'axes.titlesize': 'x-large'})
+
+    if im.shape == (182, 218, 182):
+        x = [78, 90, 100]
+        y = [82, 107, 142]
+        z = [88, 103, 107]
+    else:
+        shap = im.shape
+        x = [int(shap[0]*0.35), int(shap[0]*0.51), int(shap[0]*0.65)]
+        y = [int(shap[1]*0.35), int(shap[1]*0.51), int(shap[1]*0.65)]
+        z = [int(shap[2]*0.35), int(shap[2]*0.51), int(shap[2]*0.65)]
+    coords = (x, y, z)
+
+    labs = ['Sagittal Slice (YZ fixed)',
+            'Coronal Slice (XZ fixed)',
+            'Axial Slice (XY fixed)']
+    var = ['X', 'Y', 'Z']
+
+    idx = 0
+    for i, coord in enumerate(coords):
+        for pos in coord:
+            idx += 1
+            ax = plt.subplot(3, 3, idx)
+            ax.set_title(var[i] + " = " + str(pos))
+            if i == 0:
+                image = ndimage.rotate(im[pos, :, :,0:3], 90)
+            elif i == 1:
+                image = ndimage.rotate(im[:, pos, :,0:3], 90)
+            else:
+                image = im[:, :, pos,0:3]
+
+            print image.shape
+            if idx % 3 == 1:
+                ax.set_ylabel(labs[i])
+                ax.yaxis.set_ticks([0, image.shape[0]/2, image.shape[0] - 1])
+                ax.xaxis.set_ticks([0, image.shape[1]/2, image.shape[1] - 1])
+
+            plt.imshow(image)
+
+    fig = plt.gcf()
+    fig.set_size_inches(12.5, 10.5, forward=True)
+    return fig
+
+
+def fiber_stream(f):
+    test = f
+    print len(test)
+    fig = plt.figure(1)
+    plt.subplots(figsize=(10, 10))
+    plt.subplot(311)
+    plt.title("Y-axis vs X-axis (" + str(len(test)) + " fibers)")
+    for i in range(len(test)):
+        plt.plot(test[i][:,0], test[i][:,1])
+    plt.subplot(312)
+    plt.title("Z-axis vs X-axis (" + str(len(test)) + " fibers)")
+    for i in range(len(test)):
+        plt.plot(test[i][:,0], test[i][:,2])
+    plt.subplot(313)
+    plt.title("Z-axis vs Y-axis (" + str(len(test)) + " fibers)")
+    for i in range(len(test)):
+        plt.plot(test[i][:,1], test[i][:,2])
+    plt.tight_layout()
+    #fig = plt.show()
+    fig.savefig('tensor_streamlines.png')
+
+
+tensor2tract(struct_tensor, is_fsl):
+    if is_fsl:
+        tmp = np.copy(struct_tensor[:,:,:,3])
+        struct_tensor[:,:,:,3] = struct_tensor[:,:,:,2]
+        struct_tensor[:,:,:,2] = tmp
+    output = from_lower_triangular(struct_tensor)
+
+    evals, evecs = decompose_tensor(output)
+
+    FA = fractional_anisotropy(evals)
+
+    RGB = color_fa(FA, evecs)
+    # nb.save(nb.Nifti1Image(np.array(255 * RGB, 'uint8'), result.get_affine()), 'fsl_tensor_rgb_upper.nii.gz')
+
+    affine = result.get_affine()
+    fa = nb.Nifti1Image(np.array(255 * RGB, 'uint8'), affine)
+    im = fa.get_data()
+
+    fig = plot_rgb(im)
+    plt.savefig('tensor_field_brain.png')
+
+
+    sphere = get_sphere('symmetric724')
+    peak_indices = quantize_evecs(evecs, sphere.vertices)
+    eu = EuDX(FA.astype('f8'), peak_indices, seeds=50000, odf_vertices = sphere.vertices, a_low=0.2)
+    tensor_streamlines = [streamline for streamline in eu]
+    return tensor_streamlines
